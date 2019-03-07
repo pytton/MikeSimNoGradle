e14b4255f8022df3776b2bc7ecd0ca6be4e7c0f1
pytton <pytton@hotmail.com>
2019-03-06 22:30:22 -0600

    Figured out how to access Controllers of fxml files. Accessed and changed ask price in PositionsWindow.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,29 +5,42 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import main.java.controller.ControllerPostitionsWindow;
 import main.java.model.ClockTicker;
 
 public class Main extends Application {
 
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/SceneBuilder/PriceControlPanel.fxml"));
+
+        FXMLLoader priceControlPanelLoader = new FXMLLoader(getClass().getResource("view/SceneBuilder/PriceControlPanel.fxml"));
+        Parent pricePanelRoot =  priceControlPanelLoader.load(); //FXMLLoader.load(getClass().getResource("view/SceneBuilder/PriceControlPanel.fxml"));
         primaryStage.setTitle("Hello World");
 
 
 
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setScene(new Scene(pricePanelRoot));
         primaryStage.setX(1200);
         primaryStage.setY(50);
 
-        Parent root2 = FXMLLoader.load(getClass().getResource("view/SceneBuilder/PositionsWindow.fxml"));
+        //load the FXML file
+        FXMLLoader posWindowLoader = new FXMLLoader(getClass().getResource("view/SceneBuilder/PositionsWindow.fxml"));
+        Parent positionsWindowRoot = posWindowLoader.load();
+        //access the controller of the FXML file:
+        ControllerPostitionsWindow positionsWindowController = (ControllerPostitionsWindow)posWindowLoader.getController();
+        //set something inside the controller:
+        positionsWindowController.setAskPriceTextField(145);
 
         Stage secondStage = new Stage();
         secondStage.setX(0);
         secondStage.setY(50);
 
-        Scene positions1 = new Scene(root2);
-        
+        Scene positions1 = new Scene(positionsWindowRoot);
+
+
+
         secondStage.setScene(positions1);
         secondStage.show();
         primaryStage.show();
