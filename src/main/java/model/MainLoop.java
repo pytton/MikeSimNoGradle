6d84141d048e77321f90e1e57162941d8b86f7f1
pytton <pytton@hotmail.com>
2019-06-24 18:33:54 +0200

    works. TimedRunnable not working

--- src/main/java/model/MainLoop.java
+++ src/main/java/model/MainLoop.java
@@ -16,6 +16,9 @@ public class MainLoop extends Thread {
     PriceServer priceServer;
     private ControllerPositionsWindow controllerPositionsWindow;
 
+    TimedRunnable myTimedRunnable;
+
+
     public void setPriceServer(PriceServer priceServer) {
         this.priceServer = priceServer;
     }
@@ -35,21 +38,44 @@ public class MainLoop extends Thread {
     }
 
 
-
     private Integer count = 0;
     public static boolean interrupted;
-    public void run()  {
 
-        while (!interrupted){
+    public void run() {
+
+        myTimedRunnable = new TimedRunnable();
+
+        while (!interrupted) {
             try {
-                if (mikeGridPane != null){
+                if (mikeGridPane != null) {
 
                     Button button = mikeGridPane.getButton(8, 3);
 //                    button.setText(count.toString());
+
+//                    if(myTimedRunnable.isReady()){
+//                        myTimedRunnable.setControllerPositionsWindow(controllerPositionsWindow);
+//                        myTimedRunnable.setPriceServer(priceServer);
+//                        Platform.runLater(myTimedRunnable);
+//                    }
+
                     Platform.runLater(new Runnable() {
                         @Override
                         public void run() {
-                            controllerPositionsWindow.setSpecificButtonInMikeGridPane(8,3, "" + count);
+                            controllerPositionsWindow.setSpecificButtonInMikeGridPane(8, 3, "" + count);
+
+                            for (int i = 0; i < 20; i++) {
+                                controllerPositionsWindow.setSpecificButtonInMikeGridPane(i, 0, "" + ((Math.sqrt((count * 79 + count))) * 1) % 567);
+                                controllerPositionsWindow.setSpecificButtonInMikeGridPane(i, 1, "" + count);
+                                controllerPositionsWindow.setSpecificButtonInMikeGridPane(i, 2, "" + Math.sqrt((count * 79 + i + count)) % 7);
+                                controllerPositionsWindow.setSpecificButtonInMikeGridPane(i, 3, "" + Math.sqrt((count * 79 + i + count)) % 56);
+                                controllerPositionsWindow.setSpecificButtonInMikeGridPane(i, 4, "" + Math.sqrt((count * 73 + i + count)) % 74);
+                                controllerPositionsWindow.setSpecificButtonInMikeGridPane(i, 5, "" + Math.sqrt((count * 987 + i + count)) % 34);
+                                controllerPositionsWindow.setSpecificButtonInMikeGridPane(i, 6, "" + Math.sqrt((count * 453 + i + count)) % 9);
+                            }
+                            controllerPositionsWindow.askPriceTextField.setText(((Double) priceServer.getRealTimeAskPrice()).toString());
+                            controllerPositionsWindow.bidPriceTextField.setText(((Double) priceServer.getRealTimeBidPrice()).toString());
+
+
                         }
                     });
 
@@ -62,9 +88,9 @@ public class MainLoop extends Thread {
             }
             System.out.println("1 second tick");
             //TODO: experimenting:
-            int experimental = (int)(Math.random()*100);
+            int experimental = (int) (Math.random() * 100);
             priceServer.setExperimentalNumber(experimental);
-            controllerPositionsWindow.setExperimentalTextField(experimental);
+//            controllerPositionsWindow.setExperimentalTextField(experimental);
         }
     }
 }
